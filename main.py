792d84d9fa47a9eb1cee0053c8f83dc64bee584a
Huda <hudarashid87@gmail.com>
2024-05-26 19:14:19 +0100

    Initial commit for streamlit app for publication count

---
+++ main.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+
+from pmc_api import search_api, transform_search_result
+
+
+end_date = datetime.now()
+
+end_date_year = int(end_date.year)
+start_date_year = int(end_date.year) - 20
+
+st.title(f"Publication Counts based on the Europe PMC RESTful Web Service")
+
+search_form = st.form("search_form")
+
+text_input = search_form.text_input(label="Search for", placeholder="covid-19")
+search_option = search_form.radio(label="Search option:", key="visibility", options=["any", "title", "abstract"])
+
+# Create a double-ended datetime slider
+selected_date_range = search_form.slider(
+    "Select a year range",
+    min_value=start_date_year,
+    max_value=end_date_year,
+    value=(start_date_year, end_date_year),
+    step=1,
+)
+
+submitted = search_form.form_submit_button('Submit my search')
+
+if submitted:
+    if not text_input:
+        search_form.error(f"Please enter a search term.")
+    else:
+        search_api_results, time_taken = search_api(
+            search_term=text_input, 
+            search_option=search_option, 
+            year_range=selected_date_range
+        )
+
+        results = transform_search_result(search_api_results)
+
+        # Rendered historgram
+        years = pd.RangeIndex(start=results["start_year"], stop=results["end_year"]+1)
+        publication_counts = results["hitCounts"] 
+
+        chart_data = pd.DataFrame({
+            'Year': years,
+            'Publication Count': publication_counts
+        })
+
+        # Create the horizontal bar chart using Altair
+        horizontal_bar_chart = alt.Chart(chart_data).mark_bar().encode(
+            x=alt.X('Publication Count:Q', axis=alt.Axis(format=',d')),
+            y=alt.Y('Year:O', axis=alt.Axis(title='Year'))
+        ).properties(
+            width=700,
+            height=500,
+            title=f"Publication Counts for search term :'{text_input}' in the year {selected_date_range[0]} - {selected_date_range[1]}.\
+                 \n Time taken: {round(time_taken, 2)}"
+        )
+        
+        # Display the chart in Streamlit
+        st.altair_chart(horizontal_bar_chart, use_container_width=True)
---
+++ pmc_api.py
@@ -0,0 +1,90 @@
+import streamlit as st
+import requests
+
+from typing import Dict, List, Tuple
+from timeit import default_timer as timer
+
+
+def _transform_search_query(search_term: str, search_option: str) -> str:
+    if search_option == "any":
+        return search_term
+    if search_option == "title":
+        return f"({search_option.upper()}:{search_term})"
+    if search_option == "abstract":
+        return f"({search_option.upper()}:{search_term})"
+
+def _transform_year_range(year:int) -> str:
+    start_year_date = f"{str(year)}-01-01"
+    end_year_date = f"{str(year)}-12-31"
+
+    return f"(FIRST_PDATE:[{start_year_date} TO {end_year_date}])"
+
+@st.cache_data
+def _search_api_based_on_year(
+    search_term: str,
+    search_option: str,
+    year:int
+) -> Tuple[int, int]:
+    """
+    Return the Tuple of the hitCounts for the year.
+    """
+    _search = _transform_search_query(search_term, search_option)
+    _date_range = _transform_year_range(year)
+
+    search_query = f"{_search} AND {_date_range}"
+
+    search_url = f"https://www.ebi.ac.uk/europepmc/webservices/rest/search?query={search_query}&format=JSON&sort=CITED%20desc&pageSize=1"
+    
+    response = requests.get(search_url)
+    data = response.json()
+
+    if not data:
+        return st.warning(f"An error has occured. Please try again later.")
+
+    return year, data.get("hitCount", 0)
+
+@st.cache_data
+def search_api(
+    search_term: str,
+    search_option: str,
+    year_range:Tuple[int, int]
+) -> List[Tuple[int, int]]:
+    """
+    Return the list of each year hitCounts and the queries time taken.
+
+    """
+    start = timer()
+
+    start_year, end_year = year_range
+
+    hit_counts_list = [_search_api_based_on_year(search_term, search_option, year) for year in range(start_year, end_year + 1)]
+
+    end = timer()
+
+    return hit_counts_list, (end-start)
+
+@st.cache_data
+def transform_search_result(search_result: List[Tuple[int, int]]) -> Dict[str, any]:
+    """
+    Transform the search result into a dictionary with start_year, end_year, size of the list, and list of the hitCounts.
+    """
+    if not search_result:
+        return {
+            "start_year": None,
+            "end_year": None,
+            "size": 0,
+            "hitCounts": []
+        }
+    
+    start_year = search_result[0][0]
+    end_year = search_result[-1][0]
+    size = len(search_result)
+    hitCounts = [count for year, count in search_result]
+    
+    return {
+        "start_year": start_year,
+        "end_year": end_year,
+        "size": size,
+        "hitCounts": hitCounts
+    }
+
